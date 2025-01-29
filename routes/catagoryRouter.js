b7ad61c80a99f6d3c6ef7da19fc02ded7e5f858d
akberofh <sd85bejal@code.edu.az>
2025-01-29 14:35:21 +0400

    son commitsffsf

--- routes/catagoryRouter.js
+++ routes/catagoryRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { adminControlAuth, userControlAuth } from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
-import { catagoryAd, catagoryUpdate, deleteById, getCatagory } from "../controllers/catagoryController.js";
+import { catagoryAd, catagoryUpdate, deleteById, getByIdCatagory, getCatagory } from "../controllers/catagoryController.js";
 
 
 
 const router = express.Router();
 
 router.get('/', getCatagory)
+router.get('/:id', getByIdCatagory)
 router.post('/', upload.single('photo'),catagoryAd);
 router.put('/:id', upload.single('photo'),  catagoryUpdate);
 
