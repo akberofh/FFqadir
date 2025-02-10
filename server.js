caee4e14f75e0f4cc541b157df3aaf661ef90f92
akberofh <sd85bejal@code.edu.az>
2025-02-10 09:16:04 +0400

    sfvgdhyf

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const PORT = process.env.PORT || 5000;
 // CORS Middleware Ayarı
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://127.0.0.1:5501',
   'http://127.0.0.1:5500',
   'https://ffqadir.az',
+  'https://www.ffqadir.az',
   'https://f-fqadir-adminpanel.vercel.app'
 ];
 
